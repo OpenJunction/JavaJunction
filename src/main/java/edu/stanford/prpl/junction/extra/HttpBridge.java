143fd30298dd7f7b29c8db84d42dfe36b5efc2d7
Ben Dodson <bjdodson@gmail.com>
2009-05-21 23:18:56 +0000

    migrated Arthur/Behram's HttpBridge implementation

--- src/main/java/edu/stanford/prpl/junction/extra/HttpBridge.java
+++ src/main/java/edu/stanford/prpl/junction/extra/HttpBridge.java
@@ -1,10 +1,16 @@
 package edu.stanford.prpl.junction.extra;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
 
 public class HttpBridge extends HttpServlet {
 	/**
@@ -12,10 +18,6 @@ public class HttpBridge extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	
-	public static void main(String[] argv) {
-
-	}
 	
 	// static makes the same hashmap available
 	// to all connecting threads.
@@ -30,11 +32,11 @@ public class HttpBridge extends HttpServlet {
 	protected void doPost(HttpServletRequest req,
 			HttpServletResponse resp) {
 	
-		
 		String uri = req.getRequestURI();
 		HttpBridgePair pair;
 		if (!bridge.containsKey(uri)) {
 			pair = new HttpBridgePair();
+			bridge.put(uri, pair);
 		} else {
 			pair = bridge.get(uri);
 		}
@@ -50,11 +52,12 @@ public class HttpBridge extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req,
 			HttpServletResponse resp) {
-	
+		
 		String uri = req.getRequestURI();
 		HttpBridgePair pair;
 		if (!bridge.containsKey(uri)) {
 			pair = new HttpBridgePair();
+			bridge.put(uri, pair);
 		} else {
 			pair = bridge.get(uri);
 		}
@@ -65,10 +68,8 @@ public class HttpBridge extends HttpServlet {
 		}
 		
 		pair.setGetter(req, resp);
+		bridge.remove(uri);
 	}
-
-
-
 }
 
 class HttpBridgePair {
@@ -76,67 +77,146 @@ class HttpBridgePair {
 	private HTTPClientConnection mGetter;
 	
 	public boolean hasGetter() {
-		return (mGetter == null);
+		return (mGetter != null);
 	}
 	
 	public boolean hasPoster() {
-		return (mPoster == null);
+		return (mPoster != null);
 	}
 	
 	
 	public synchronized void setPoster(HttpServletRequest req,
 				HttpServletResponse resp) {
 
-/*
-the req object has an input stream; use getInputStream() to get it.
-Also get the content length as: getContentLength() and type as getContentType().
-
-*/
+		/*
+		the req object has an input stream; use getInputStream() to get it.
+		Also get the content length as: getContentLength() and type as getContentType().
+		*/
 
 		mPoster = new HTTPClientConnection(req,resp);
-		
-		while (null == mGetter){
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				
-				e.printStackTrace();
+
+		System.out.println("Set Poster");
+		if(hasGetter()) {
+			System.out.println("Poster notifying...");
+			this.notify();
+		} else {
+			while (null == mGetter){
+				try {
+					System.out.println("Poster waiting...");
+					this.wait();
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
 			}
 		}
-		
-		bridgeConnections();
 
+		System.out.println("Poster done waiting");
+		// only one side bridges connection:
+		bridgeConnections();
 	}
 
 	public synchronized void setGetter(HttpServletRequest req,
 				HttpServletResponse resp) {
 		
+		System.out.println("Set Getter");
 		mGetter = new HTTPClientConnection(req,resp);
 		
-		if(null != mPoster) {
+		if(hasPoster()) {
+			System.out.println("Getter notifying...");
 			this.notify();
+		} else {
+			while (null == mPoster){
+				try {
+					System.out.println("Getter waiting...");
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-
+		
+		System.out.println("Getter done waiting");
 	}
 
-	
-	
 	/**
 	 * Does the work of actually bridging our connections,
 	 * given a GET and POST request.
 	 */
 	private void bridgeConnections() {
 		// bridge poster.req.getInputStream()
+			
 		// and
 		// getter.resp.getOutputStream()
-		
-	}
 
-	
-	
-	
-	
-	
+		//need to get header
+		//send other header
+
+		//get InputStream
+		//send InputStream
+
+		try{
+			String postType = mPoster.request.getContentType();
+			//int postLength = mPoster.request.getContentLength();			
+						
+			ServletInputStream in = mPoster.request.getInputStream();		
+			ServletOutputStream out = mGetter.response.getOutputStream();
+
+			if(postType.indexOf("multipart/form-data") == -1){
+				//mGetter.response.setContentType("application/pdf");
+				int i = in.read();
+				while(i != -1){
+					out.write((char) i);
+					i = in.read();
+				}
+			}
+			else{
+				StringBuffer line = new StringBuffer();
+				StringBuffer preamble = new StringBuffer();
+				int i = in.read();
+				while(i != -1){
+					if((char)i == '\n'){
+						String s = line.toString();	
+						String CONTENT_TYPE = "Content-Type: ";
+						if(s.indexOf(CONTENT_TYPE) != -1){
+							String type = s.substring(CONTENT_TYPE.length());
+							if(!type.equals("multipart/form-data")){
+								mGetter.response.setContentType(type);
+								preamble.append((char)i);
+								break;
+							}
+						}
+						line = new StringBuffer();
+					}
+					line.append((char)i);
+					preamble.append((char)i);
+					i = in.read();
+				}
+				out.print(preamble.toString());
+				i = in.read();
+				while(i != -1){
+					out.write((char)i);
+					i = in.read();
+				}
+			}
+			
+			//StringBuffer s = new StringBuffer();
+			/*int i = inStream.read();
+			while(i != -1){
+				outStream.print((char)i);
+				//s.append((char)i);
+				i = inStream.read();
+			}*/
+
+			in.close();
+			out.close();
+
+		}
+		catch (Exception e)
+		{
+		//print some type of error here.
+		}	
+	}
 	
 	class HTTPClientConnection {
 		public HttpServletRequest request;
