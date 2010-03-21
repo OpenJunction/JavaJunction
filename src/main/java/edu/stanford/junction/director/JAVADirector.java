94d627d3fa7461eb0c33538863ad013310f2abaf
Ben Dodson <bjdodson@gmail.com>
2010-03-21 18:00:39 +0000

    director accepts new format of casting

--- src/main/java/edu/stanford/junction/director/JAVADirector.java
+++ src/main/java/edu/stanford/junction/director/JAVADirector.java
@@ -12,12 +12,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import edu.stanford.junction.JunctionMaker;
+import edu.stanford.junction.api.activity.ActivityScript;
+import edu.stanford.junction.api.activity.JunctionActor;
 import edu.stanford.junction.api.activity.JunctionService;
 import edu.stanford.junction.api.messaging.MessageHandler;
 import edu.stanford.junction.api.messaging.MessageHeader;
+import edu.stanford.junction.provider.xmpp.XMPPSwitchboardConfig;
 
 /**
  * Sample message to launch a JAR:
@@ -25,59 +29,130 @@ import edu.stanford.junction.api.messaging.MessageHeader;
  * @author Ben
  *
  */
-public class JAVADirector extends JunctionService {
+public class JAVADirector extends JunctionActor {
+
+	// TODO: The maker is used (poorly) to look up an activity script.
+	private static JunctionMaker mMaker = null;
+	private List<Activity>mActivities;
+
+	public JAVADirector() {
+		super("director");
+		mActivities = new ArrayList<Activity>();
+	}
 
 	@Override
 	public void onMessageReceived(MessageHeader header, JSONObject message) {
-			try {
-				
-				URI activityURI = new URI(message.getString("activity"));
-				
-				if (message.has("jar")) {
-					URL jarURL = new URL(message.getString("jar"));
-					launchJAR(jarURL,activityURI);
-				} else {
-					String className = message.getString("serviceName");
-					launchService(activityURI,className);
+		try {
+			if (message.has("action")) {
+				String action = message.getString("action");
+				if ("list".equals(action)) {
+					JSONArray procs = new JSONArray();
+					for (int i = mActivities.size()-1; i>=0; i--) {
+						Activity activity = mActivities.get(i);
+						try {
+							activity.process.exitValue();
+							// If we get this far, the process is terminated.
+							mActivities.remove(i);
+						} catch (Exception e) {
+							// No exit value means its still running.
+							JSONObject obj = new JSONObject();
+							obj.put("activity", activity.uri.toString());
+							procs.put(obj);
+						}
+					}
+					JSONObject msg = new JSONObject();
+					msg.put("activities",procs);
+					header.getReplyTarget().sendMessage(msg);
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
+
+				else if ("info".equals(action)) {
+					// return Junction version, platform(s), and "hints" (headless, bigscreen, etc)
+					// also a nickname.
+
+					// maybe other known directors? owner info?
+
+					// HINTS:
+					// headless ~ server
+					// bigscreen ~ TV or monitor attached
+					// mobile ~ phone
+					// nouser ~ no direct user input (bigscreen / headless)
+					// keyboard? mouse?
+				}
+				
+				else if ("cast".equals(action)) {
+					// TODO: support different platforms ("java", "web")
+					String activityString = message.getString("activity");
+					URI activityURI = new URI(activityString);
+					
+					// TODO: clean this up.
+					ActivityScript script = mMaker.getActivityDescription(activityURI);
+					int p = activityString.indexOf("role=");
+					if (p < 0) {
+						System.out.println("Invitation does not specify a role.");
+						return;
+					}
+					
+					String role = activityString.substring(p+5);
+					if (role.contains("&")) {
+						int q = role.indexOf("&");
+						role = role.substring(0,q);
+					}
+					
+					JSONObject spec = script.getRoleSpec(role);
+					JSONObject platforms = spec.getJSONObject("platforms");
+					if (platforms.has("java")) {
+						JSONObject javaplat = platforms.getJSONObject("java");
+						if (javaplat.has("jar")) {
+							URL jarURL = new URL(javaplat.getString("jar"));
+							launchJAR(jarURL,activityURI);
+						} else {
+							System.out.println("Warning: JAVA platform specified but no JAR found.");
+						}
+					} else {
+						String className = message.getString("serviceName");
+						launchService(activityURI,className);
+					}
+				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	// currently no multi-thread support
-	private synchronized void launchJAR(URL jarURL, URI activityURI) {
+	private void launchJAR(URL jarURL, URI activityURI) {
 		final String JAR_PATH = "jars/";
-		
+
 		String jarName = JAR_PATH + "/" + jarURL.getPath().substring(1).replace("/", "-");
 		File jarFile = new File(jarName);
-		if (!jarFile.exists()) {
-			File tmpFile = new File(JAR_PATH+"/inbound.jar.tmp");
+		File tmpFile = new File(jarName+".tmp");
+		if (!jarFile.exists() && !tmpFile.exists()) {
 			try {
 				FileOutputStream out = new FileOutputStream(tmpFile);
-		        
-		        InputStream in = jarURL.openStream();
-		        byte[] buf = new byte[4 * 1024];
-		        int bytesRead;
-		        while ((bytesRead = in.read(buf)) != -1) {
-		          out.write(buf, 0, bytesRead);
-		        }
-		        in.close();
-		        out.close();
 
-		        boolean res = tmpFile.renameTo(jarFile);
-		        
+				InputStream in = jarURL.openStream();
+				byte[] buf = new byte[4 * 1024];
+				int bytesRead;
+				while ((bytesRead = in.read(buf)) != -1) {
+					out.write(buf, 0, bytesRead);
+				}
+				in.close();
+				out.close();
+
+				boolean res = tmpFile.renameTo(jarFile);
+				if (!res) {
+					throw new Exception("Could not rename file.");
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 				return;
 			}
 		}
-		
+
 		if (!jarFile.exists()) {
 			System.out.println("Failed to get JAR file " + jarFile.getName());
 			return;
 		}
-		
+
 		// Launch the new JVM
 		try {
 			List<String>command = new ArrayList<String>();
@@ -85,19 +160,21 @@ public class JAVADirector extends JunctionService {
 			command.add("-jar");
 			command.add(jarFile.getAbsolutePath());
 			command.add(activityURI.toString());
-			
+
 			System.out.println("Executing: " + command.toString());
 			ProcessBuilder pb = new ProcessBuilder(command);
 			pb.directory(jarFile.getParentFile());
-			
+
 			Process p = pb.start();
+			// TODO: make sure it worked
+			mActivities.add(new Activity(activityURI,p));
 		} catch (Exception e) {
 			System.out.println("failed to launch JAR.");
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	private void launchService(URI activityURI, String className) {	
 		Class c = null;
 		try {
@@ -105,8 +182,8 @@ public class JAVADirector extends JunctionService {
 		} catch (Exception e) {
 			System.out.println("Could not find class for service " + className + ".");
 		}
-		
-		
+
+
 		JunctionService service = null;
 		Method method = null;
 		try {
@@ -115,8 +192,8 @@ public class JAVADirector extends JunctionService {
 		} catch (Exception e) {
 			System.out.println("No newInstance method found for " + c + ".");
 		}
-		
-		
+
+
 		String queryPart = activityURI.getQuery(); 
 		System.out.println("query part is " + queryPart);
 		String localRole = "Unknown";
@@ -127,32 +204,96 @@ public class JAVADirector extends JunctionService {
 				localRole = localRole.substring(0,i);
 			}
 		}
-		
+
 		System.out.println("Setting service role to " + localRole);
 		service.setRole(localRole);
-		
+
 		System.out.println("service actorID is " + service.getActorID());
 		//JunctionMaker.getInstance().newJunction(activityURI,service);					
 	}
-	
 
-	@Override
-	public String getServiceName() {
-		return "ServiceFactory";
-	}
 
-	
 	public static void main(String[] argv) {
 		String switchboard="prpl.stanford.edu";
+		String sessionID = "jxservice";
 		
-		JunctionService waiter = new JAVADirector();
-		waiter.register(switchboard);
-		synchronized(waiter){
+		URI activityURI = null;
+		try {
+			activityURI = new URI("junction://"+switchboard+"/"+sessionID);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		XMPPSwitchboardConfig config = new XMPPSwitchboardConfig(switchboard);
+		mMaker = JunctionMaker.getInstance(config);
+		
+		JunctionActor director = new JAVADirector();
+		mMaker.newJunction(activityURI, director);
+		
+		synchronized(director){
 			try {
-				waiter.wait();
+				director.wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}	
+}
+
+class Activity {
+	public URI uri;
+	public Process process;
+
+	public Activity(URI uri,Process process) {
+		this.uri = uri;
+		this.process = process;
+	}
+}
+
+/**
+ * 
+ * http://javaxden.blogspot.com/2007/09/launch-web-browser-through-java.html
+ *
+ */
+class BrowserControl{ 
+	/**
+	 * Method to Open the Browser with Given URL
+	 * @param url
+	 */
+	public static Process openUrl(String url){
+		String os = System.getProperty("os.name");
+		Runtime runtime=Runtime.getRuntime();
+		try{
+			// Block for Windows Platform
+			if (os.startsWith("Windows")){
+				String cmd = "rundll32 url.dll,FileProtocolHandler "+ url;
+				Process p = runtime.exec(cmd);
+				return p;
+			}
+			//Block for Mac OS
+			else if(os.startsWith("Mac OS")){
+				Class fileMgr = Class.forName("com.apple.eio.FileManager");
+				Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[] {String.class});
+				openURL.invoke(null, new Object[] {url});
+				return null; // TODO find a better way
+			}
+			//Block for UNIX Platform 
+			else {
+				String[] browsers = {"firefox", "chrome", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+				String browser = null;
+				for (int count = 0; count < browsers.length && browser == null; count++)
+					if (runtime.exec(new String[] {"which", browsers[count]}).waitFor() == 0)
+						browser = browsers[count];
+				if (browser == null)
+					throw new Exception("Could not find web browser");
+				else 
+					return runtime.exec(new String[] {browser, url});
+			}
+		}catch(Exception x){
+			System.err.println("Exception occurd while invoking Browser!");
+			x.printStackTrace();
+			return null;
+		}
+	}
 }
