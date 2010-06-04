556e67b8771b3e8465a12f2e28d27e708312d00d
Ben Dodson <bjdodson@gmail.com>
2010-06-04 01:07:42 +0000

    added demo of SystemExtra

---
+++ src/main/java/edu/stanford/junction/sample/extra/System.java
@@ -0,0 +1,46 @@
+package edu.stanford.junction.sample.extra;
+
+import org.json.JSONObject;
+
+import edu.stanford.junction.api.activity.JunctionExtra;
+import edu.stanford.junction.api.messaging.MessageHeader;
+
+/**
+ * Handles internal System requests. This may include handover to OOB-transport
+ * (or, at least, serves as an example for how to do OOB transport)
+ * @author bjdodson
+ *
+ */
+public class System extends JunctionExtra {	
+		protected static final String JX_SYSTEM_NS = "JXSYSTEMMSG";
+		
+		public System() {
+		
+		}
+
+		@Override
+		public boolean beforeSendMessage(JSONObject msg) {
+			// TODO: if message has JXSYSTEMMSG flag, don't allow.
+			return super.beforeSendMessage(msg);
+		}
+		
+		@Override
+		public boolean beforeOnMessageReceived(MessageHeader h, JSONObject msg) {
+			if (msg.has(JX_SYSTEM_NS)) {
+				// do something
+				return false;
+			}
+			
+			return true;	
+		}
+		
+		// TODO: in case of out-of-band transport, some external
+		// process triggers a call to this.getActor().onMessageReceived(h,msg);
+		// make sure that runs through the appropriate Extras.
+		
+		public static void main(String ... args) {
+			new System().test();
+		}
+		
+		// public void test() {} // overrides testing.  geucy.
+}
