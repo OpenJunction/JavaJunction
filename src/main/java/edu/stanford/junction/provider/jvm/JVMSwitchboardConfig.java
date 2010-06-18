8732cfc47c4491f2f48c77aa019c8bbcf8de2dc8
Ben Dodson <bjdodson@gmail.com>
2010-06-18 04:57:32 +0000

    added notes

--- src/main/java/edu/stanford/junction/provider/jvm/JVMSwitchboardConfig.java
+++ src/main/java/edu/stanford/junction/provider/jvm/JVMSwitchboardConfig.java
@@ -15,5 +15,26 @@ import edu.stanford.junction.SwitchboardConfig;
  *
  */
 public class JVMSwitchboardConfig implements SwitchboardConfig {
+	/* an email..
 
+ 
+I had a little fun tonight and wrote a new Switchboard implementation-- JVMSwitchboard. It's used to send messages to actors located within a single JVM. The implementation basically has a set of actors in a session, and routes the JSON message to all actors.
+
+It will be quite buggy, for example:
+ * all actors share the mutable JSONObject
+ * extras don't currently work
+ * onActivityCreate not triggered
+ * many more
+
+Mainly its a proof of concept for how to write other switchboards, and it exposed a lot of things I am really not happy with in the API. So if I get time and energy (NOT LIKELY!) I would like to work out those API hickups. For example:
+ * Provider implementation shouldn't see so much of the Extras API
+ * More clarity with how sessionIDs are created
+ * much more
+
+A fun thing it exposes though is how cool it would be to have a network of switchboards. So we have a JVMSwitchboard, LocalhostSwitchboard, OpenflowSwitchboard, and XMPPSwitchboard. The system then auto-figures out which switchboard or switchboards to use to glue together a set of actors. If all clients are on the same box, use LocalhostSwitchboard. If on the same network, OpenflowSwitchboard. If a remote actor joins, cut over to XMPPSwitchboard. If two are on the same network and one remote, federate between OpenflowSwitchboard and XMPPSwitchboard.
+
+This would be hard, but I think PHDs are supposed to be hard ;)
+
+
+	 */
 }
