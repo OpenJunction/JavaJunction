23a2dd60bd17c3681398f0909db5978848ef4e8f
Ben Dodson <bjdodson@gmail.com>
2010-07-14 22:41:31 +0000

    added documentation on SwitchboardConfig

--- src/main/java/edu/stanford/junction/SwitchboardConfig.java
+++ src/main/java/edu/stanford/junction/SwitchboardConfig.java
@@ -1,3 +1,12 @@
 package edu.stanford.junction;
 
+import edu.stanford.junction.provider.JunctionProvider;
+
+/**
+ * A class implementing this interface can be instantiated and loaded by
+ * {@link JunctionMaker#getInstance(SwitchboardConfig)}. Subsequent calls to
+ * the resulting {@link JunctionMaker} object's {@code newJunction} methods
+ * will create {@link Junction} objects using the SwitchboardConfig's associated
+ * {@link JunctionProvider} class as a factory.
+ */
 public interface SwitchboardConfig {}
